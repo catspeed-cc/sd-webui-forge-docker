fcab2184d58c44886be64b3473fdc96bf28505f2
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2025-07-31 11:37:30 -0400

    checkpoint: part way adding notes

---
+++ docker-start-containers.sh
@@ -0,0 +1,10 @@
+# placeholder
+
+# going to have to do \"docker ps -a\" and search for sd-forge container
+# oof not fun start on this one
+
+DOCKER_PSA=$(docker ps -a)
+
+echo "Debug: DOCKER_PSA:[$DOCKER_PSA]"
+
+
