7e0b25a69ff8a058d02a1f8e82e8f06ae84cbd64
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2025-08-01 11:20:35 -0400

    restored old files!

---
+++ docker-init-cpu-only.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# simple should not be config req'd
+docker compose up
---
+++ docker-stop-containers.sh
@@ -0,0 +1,20 @@
+# placeholder
+
+# going to have to do \"docker ps -a\" and search for sd-forge container
+# oof not fun start on this one
+
+DOCKER_PSA_LIST=$(docker ps -a --format '{{.Names}}' | grep "sd-forge")
+
+echo "Debug: DOCKER_PSA_LIST:[$DOCKER_PSA_LIST]"
+
+# Loop through each line
+while read line; do
+    echo "Stopping docker container: $line"
+    docker stop $line
+done <<< "$DOCKER_PSA_LIST"
+
+echo "docker ps -a output:"
+docker ps -a
+
+echo "Docker containers stopped."
+
