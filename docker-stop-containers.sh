e8528c506e1f7c7bf232b4166e290312aeceabab
MooCat <43627985+mooleshacat@users.noreply.github.com>
2025-08-02 04:36:01 -0400

    Merge: `fix-sauces` into `development` (#9)
    * updated sauces, fixed destroy scripts
    * fixed success line, added space around it to be more visible
    * updated init scripts to use -d to detach container after start
    * update gitignore to exclude release helper scripts
    * updated .gitignore to not exclude my sauces/ directory!
    * added new sauce directory, and first sauces archive v1.0.0-sauce.tar.gz - using my helper script :)

--- docker-stop-containers.sh
+++ docker-stop-containers.sh
@@ -1,16 +1,14 @@
-# placeholder
+#!/usr/bin/env bash
 
-# going to have to do \"docker ps -a\" and search for sd-forge container
-# oof not fun start on this one
+# docker-stop-container.sh: stop all sd-forge containers
 
+# Get the list of ALL containers `docker ps -a`
 DOCKER_PSA_LIST=$(docker ps -a --format '{{.Names}}' | grep "sd-forge")
 
-echo "Debug: DOCKER_PSA_LIST:[$DOCKER_PSA_LIST]"
-
-# Loop through each line
-while read line; do
-    echo "Stopping docker container: $line"
-    docker stop $line
+# Loop through each container
+while read container; do
+    echo "Stopping docker container: $container"
+    docker stop $container
 done <<< "$DOCKER_PSA_LIST"
 
 echo "docker ps -a output:"
