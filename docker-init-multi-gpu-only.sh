8bb69d5733e47c86751fc2d33654706c6dab9698
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2025-08-02 04:01:08 -0400

    updated init scripts to use -d to detach container after start

--- docker-init-multi-gpu-only.sh
+++ docker-init-multi-gpu-only.sh
@@ -3,4 +3,4 @@
 # docker-init-multi-gpu-only.sh: create and start container for single GPU of many only
 
 # simple init but there is config in related docker-compose file(s)
-docker compose -f docker-compose.yaml -f docker-compose.multi-gpu.nvidia.yaml up
+docker compose -f docker-compose.yaml -f docker-compose.multi-gpu.nvidia.yaml up -d
