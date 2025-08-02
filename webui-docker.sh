f03fa92eff6ced8245bf84d3f60ee2f61e0c14e2
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2025-08-02 15:15:45 -0400

    fixed logic/precedence issues from previous commit

--- webui-docker.sh
+++ webui-docker.sh
@@ -40,22 +40,33 @@ fi
 #
 
 # change to work directory ("WD")
-cd /app/webui
+cd /app
 
-# git clone the requirement/dependency
+# RATHER than implement extensive logic to only do the deps if they do not exist, 
+# we assume the user only runs init when initializing container, so we can just
+# rm -r the directory and fetch it again. Quick,clean, simple
+rm -r webui
+# mooleshacat brb
 git clone https://github.com/lllyasviel/stable-diffusion-webui-forge webui
 
+# change into the webui dir
+cd webui/
+
 # pip install commands ALL ARE CHAINED TOGETHER BE CAREFUL EDITING THIS
-pip3 install --root-user-action ignore torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 && \
-pip3 install --root-user-action ignore -r requirements_versions.txt && \
-pip3 install --root-user-action ignore joblib && \
-pip3 install --root-user-action ignore --upgrade pip && \
-pip install --root-user-action ignore --upgrade pip && \
-pip3 install --root-user-action ignore --force-reinstall --no-deps "setuptools>=62.4"
+pip3 install --force-reinstall --no-deps --no-cache-dir --root-user-action ignore torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 && \
+pip3 install --force-reinstall --no-deps --no-cache-dir --root-user-action ignore -r requirements_versions.txt && \
+pip3 install --force-reinstall --no-deps --no-cache-dir --root-user-action ignore joblib && \
+pip3 install --force-reinstall --no-deps --no-cache-dir --root-user-action ignore --upgrade pip && \
+pip3 install --force-reinstall --no-deps --no-cache-dir --root-user-action ignore --upgrade pip && \
+pip3 install --force-reinstall --no-deps --no-cache-dir --root-user-action ignore "setuptools>=62.4"
+
+mkdir -p /app/webui/repositories
+cd /app/webui/repositories
+
+# clobber all three repo dirs
+rm -r stable-diffusion-webui-assets/ huggingface_guess/ BLIP/
 
 # modules/launch_utils.py contains the repos and hashes
-mkdir -p /app/webui/repositories && \
-cd /app/webui/repositories && \
 git clone --config core.filemode=false https://github.com/AUTOMATIC1111/stable-diffusion-webui-assets.git && \
 git clone --config core.filemode=false https://github.com/lllyasviel/huggingface_guess.git && \
 git clone --config core.filemode=false https://github.com/salesforce/BLIP.git
@@ -80,7 +91,7 @@ git checkout 84826248b49bb7ca754c73293299c4d4e23a548d
 cd /app/webui/repositories/BLIP && \
 git checkout 48211a1594f1321b00f14c9f7a5b4813144b2fb9 && \
 sed -i 's/transformers==4\.15\.0/transformers==4.46.1/g' /app/webui/repositories/BLIP/requirements.txt && \
-pip3 install --root-user-action ignore -r requirements.txt
+pip3 install --force-reinstall --no-deps --no-cache-dir --root-user-action ignore -r requirements.txt
 
 # KEEP THIS FOR REFERENCE FOR IDIOT :)
 # modules/launch_utils.py contains the repos and hashes
