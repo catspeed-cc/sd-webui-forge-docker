29d2ff2ea7ef9fb5c0f638ed6843d112947d2ab3
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2025-08-02 03:44:15 -0400

    updated sauces, fixed destroy scripts

--- docker-start-containers.sh
+++ docker-start-containers.sh
@@ -1,16 +1,14 @@
-# placeholder
+#!/usr/bin/env bash
 
-# going to have to do \"docker ps -a\" and search for sd-forge container
-# oof not fun start on this one
+# docker-start-containers.sh: start all sd-forge containers
 
+# Get the list of ALL containers `docker ps -a`
 DOCKER_PSA_LIST=$(docker ps -a --format '{{.Names}}' | grep "sd-forge")
 
-echo "Debug: DOCKER_PSA_LIST:[$DOCKER_PSA_LIST]"
-
-# Loop through each line
-while read line; do
-    echo "Starting docker container: $line"
-    docker start $line
+# Loop through each container
+while read container; do
+    echo "Starting docker container: $container"
+    docker start $container
 done <<< "$DOCKER_PSA_LIST"
 
 echo "docker ps output"
