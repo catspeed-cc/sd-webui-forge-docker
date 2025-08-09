09435b91b470872d3f759e7e02b2ecfa6d57584f
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2025-08-08 20:53:04 -0400

    fixed secretsauce.sh

--- docker/sauce_scripts/docker-reinstall-container-deps.sh
+++ docker/sauce_scripts/docker-reinstall-container-deps.sh
@@ -9,11 +9,11 @@ DOCKER_PSA_LIST=$(docker ps -a --format '{{.Names}}' | grep "sd-forge")
 # Loop through each container
 while read container; do
     echo "Reinstalling deps on docker container: $container"
-    docker exec ${container} /app/sauces/secretsauce.sh
+    docker exec ${container} /app/webui/docker/secret_sauce_baked_into_docker_image/secretsauce.sh
 done <<< "$DOCKER_PSA_LIST"
 
-./docker-stop-containers.sh
-./docker-start-containers.sh
+/app/webui/docker/secret_sauce/docker-stop-containers.sh
+/app/webui/docker/secret_sauce/docker-start-containers.sh
 
 echo ""
 echo "all containers have reinstalled their dependencies"
